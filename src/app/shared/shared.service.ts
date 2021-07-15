5925d544e6b4e361796218c169c1f7d05f70ce5b
Kuurcha <yhyr@mail.ru>
2021-07-15 09:14:21 +0300

    Put added for all entities Small interface tweaks

---
+++ src/app/shared/shared.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import {BehaviorSubject, Subject} from 'rxjs';
+import {HttpErrorResponse} from "@angular/common/http";
+
+@Injectable()
+export class SharedService {
+
+  public stateOfCheckBox: string = " "
+
+
+}
